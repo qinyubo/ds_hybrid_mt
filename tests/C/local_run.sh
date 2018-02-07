03bc229ad235aa0cf535d77c9d55b08bf2ac9c80
qinyubo <qybo123@gmail.com>
2018-02-07 13:51:32 -0500

    This branch is used for hybrid concurrent staging development

---
+++ tests/C/local_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+DIR=.
+CONF_DIMS=256
+
+rm -f conf cred dataspaces.conf srv.lck
+
+echo "## Config file for DataSpaces
+ndim = 2
+dims = $CONF_DIMS, $CONF_DIMS
+
+max_versions = 1
+lock_type = 2
+" > dataspaces.conf
+
+#./dataspaces_server -s 4 -c 72 & sleep 2
+
+#./test_writer DATASPACES 64 3 4 4 4 256 256 256 2 1 > test_writer.out 2>&1 &
+#./test_reader DATASPACES 8 3 4 2 1 256 512 1024 2 2 > test_reader.out 2>&1 &
+
+mpirun -n 1 ./dataspaces_server -s 1 -c 2 >& $DIR/server_$CONF_DIMS.log & SERVER_PID=$!
+echo "SERVER_PID: $SERVER_PID"
+sleep 2
+
+mpirun -n 1 $DIR/test_writer DATASPACES 1 2 1 1 256 256 2 1 > $DIR/writer_$CONF_DIMS.log 2>&1 &
+mpirun -n 1 $DIR/test_reader DATASPACES 1 2 1 1 256 256 2 2 > $DIR/reader_$CONF_DIMS.log 2>&1 & READER_PID=$!
+#mpirun -n 4 $DIR/test_writer DATASPACES 4 2 4 1 512 2048 5 1 > $DIR/writer_$CONF_DIMS.log 2>&1 &
+#mpirun -n 1 $DIR/test_reader DATASPACES 1 2 1 1 2048 2048 5 2 > $DIR/reader_$CONF_DIMS.log 2>&1 &
+#mpirun -n 4 $DIR/test_writer DATASPACES 4 2 4 1 256 1024 5 1 > $DIR/writer_$CONF_DIMS.log 2>&1 &
+#mpirun -n 1 $DIR/test_reader DATASPACES 1 2 1 1 1024 1024 5 2 > $DIR/reader_$CONF_DIMS.log 2>&1 &
+
+wait $READER_PID
+
+kill -9 $SERVER_PID
---
+++ tests/C/submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#sbatch /home1/yq47/code/dataspace/dataspaces_mt/tests/C/run_job.sh
+sbatch /home1/yq47/code/dataspace/ds_hybrid_mt/tests/C/simple_test.sh
+echo "Submited!"
